c99e830568fd2e2687620b05f1de46f77ebf2e56
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-08-04 22:24:02 +0300

    Message List Route Created

---
+++ ai-fitness-coach/app/api/message/list/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+import OpenAI from "openai"
+
+export async function POST(req: Request) {
+    const { threadID } = await req.json()
+
+    if(!threadID) {
+        return NextResponse.json(
+            { error: "threadID is required", success: false},
+            { status: 400}
+        )
+    }
+
+    const openai = new OpenAI
+
+    try {
+        const messages = await openai.beta.threads.messages.list(threadID)
+
+        console.log("from openai messages", messages)
+
+        return NextResponse.json({messages, success: true}, {status: 200})
+    }
+    catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { error: "Something went wrong", success: false},
+            { status: 500}
+        )
+    }
+}
