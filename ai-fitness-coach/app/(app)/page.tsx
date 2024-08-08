43971dacc4d180f31bbc99a346b4b1deed60ae58
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-08-08 21:14:06 +0300

    Working on AI ChatPage

--- ai-fitness-coach/app/(app)/page.tsx
+++ ai-fitness-coach/app/(app)/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 function ChatPage() {
+  const [fetching, setFetching] = useState(true)
   return (
-    <div>
-      ChatPage
+    <div className='w-screen h-screen flex flex-col bg-blue-950 text-yellow-400'>
+      {fetching && <div className='text-center font-bold'>Fetching...</div>}
     </div>
   )
 }
