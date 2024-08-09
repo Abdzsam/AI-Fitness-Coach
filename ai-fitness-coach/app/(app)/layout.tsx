5d381e27ae1b436af30d21d59db4db5c3b26a12c
Abdzsam <127457343+Abdzsam@users.noreply.github.com>
2024-08-09 14:39:27 +0300

    Working on Chat Page

--- ai-fitness-coach/app/(app)/layout.tsx
+++ ai-fitness-coach/app/(app)/layout.tsx
@@ -1,8 +1,41 @@
 "use client"
 import Navbar from "@/components/Navbar";
+import axios from "axios";
+import { UserThread } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 export default function AppLayout({children}: Readonly<{children: React.ReactNode}>) {
-    return (
+    
+  const [userThread, setUserThread] = useState<UserThread | null>(null)
+
+  useEffect(() => {
+
+    async function getUserThread() {
+      try {
+        const response = await axios.get<{
+      success: boolean,
+      message?: string,
+      userThread: UserThread
+      }>("/api/user-thread")
+      
+      if(!response.data.success  || !response.data.userThread){
+        console.error(response.data.message ?? "Unknown error.")
+        setUserThread(null)
+      }
+
+      setUserThread(response.data.userThread)
+      }
+      catch (error) {
+        console.error(error)
+        setUserThread(null)
+      }
+      
+    }
+    
+    getUserThread()
+  },[])
+
+  return (
       <div className="flex flex-col w-full h-full">
         <Navbar/>
         {children}
--- ai-fitness-coach/app/(app)/page.tsx
+++ ai-fitness-coach/app/(app)/page.tsx
@@ -1,11 +1,21 @@
 "use client"
+import { Threads } from 'openai/src/resources/beta/index.js'
 import React, { useState } from 'react'
 
 function ChatPage() {
   const [fetching, setFetching] = useState(true)
+  const [messages, setMessages] = useState<Threads[]>([])
+
+  const fetchmessages = async () => {
+    
+  }
+
   return (
     <div className='w-screen h-screen flex flex-col bg-blue-950 text-yellow-400'>
-      {fetching && <div className='text-center font-bold'>Fetching...</div>}
+      <div className='flex-grow overflow-y-hidden p-8 space-y-2'>
+        {fetching && <div className='text-center font-bold'>Fetching...</div>}
+        {messages.length === 0 && !fetching && (<div className='text-center font-bold'>No Messages...</div>)}
+      </div>
     </div>
   )
 }
